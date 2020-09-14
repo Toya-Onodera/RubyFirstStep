57808a1dfda112febbc4eed90386a1bfddcd8baa
Toya-Onodera <gtrzexel@gmail.com>
2020-09-14 15:47:12 +0900

    ランダムな玉の出現確率を調べるプログラム

---
+++ RandomBalls.rb
@@ -0,0 +1,14 @@
+# 「赤玉」「青玉」「黒玉」「緑玉」「黄玉」5 つの玉をランダムに取り出す
+# 1000回繰り返したときにそれぞれの玉の出現する確率を調べる
+
+balls = %w[赤玉 青玉 黒玉 緑玉 黄玉]
+randomBalls = []
+
+(1..1000).each do |i|
+  randomBalls << balls.sample
+end
+
+balls.each do |ball|
+  percent = (randomBalls.count(ball) / 1000.0) * 100
+  print("#{ball}: #{percent}%\n")
+end
