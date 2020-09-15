cebc610fc263de743d31cc1a499f1597733468ce
Toya-Onodera <gtrzexel@gmail.com>
2020-09-16 01:06:41 +0900

    命名規則的では変数名はスネークケースらしいです

--- RandomBalls.rb
+++ RandomBalls.rb
@@ -2,13 +2,13 @@
 # 1000回繰り返したときにそれぞれの玉の出現する確率を調べる
 
 balls = %w[赤玉 青玉 黒玉 緑玉 黄玉]
-randomBalls = []
+random_balls = []
 
 (1..1000).each do |i|
-  randomBalls << balls.sample
+  random_balls << balls.sample
 end
 
 balls.each do |ball|
-  percent = (randomBalls.count(ball) / 1000.0) * 100
+  percent = (random_balls.count(ball) / 1000.0) * 100
   print("#{ball}: #{percent}%\n")
 end
